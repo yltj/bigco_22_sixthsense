d2ec1e9ed6bf785be9944716716a6bfe5bf73d6c
Yi Liu <lylvxm@gmail.com>
2022-05-05 20:44:06 -0400

    recording matches book

--- Spotiobook/Controllers/RecordingsViewController.swift
+++ Spotiobook/Controllers/RecordingsViewController.swift
@@ -11,6 +11,7 @@ class RecordingsViewController: UITableViewController {
 
   var book: Book!
   var audioplayer: AVAudioPlayer?
+  var avplayer: AVPlayer?
   // MARK: Properties
   var items: [Recording] = []
   var user: User?
@@ -19,6 +20,7 @@ class RecordingsViewController: UITableViewController {
   let ref = Database.database().reference(withPath: "recordings")
   var refObservers: [DatabaseHandle] = []
   var handle: AuthStateDidChangeListenerHandle?
+  var playing: Bool = false
 
   override var preferredStatusBarStyle: UIStatusBarStyle {
     return .lightContent
@@ -27,7 +29,6 @@ class RecordingsViewController: UITableViewController {
   // MARK: UIViewController Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
-
     self.tableView.register(UITableViewCell.classForKeyedArchiver(), forCellReuseIdentifier: "RecordingCell")
     tableView.allowsMultipleSelectionDuringEditing = false
     onlineUserCount = UIBarButtonItem(
@@ -38,22 +39,40 @@ class RecordingsViewController: UITableViewController {
     onlineUserCount.tintColor = .white
     navigationItem.leftBarButtonItem = onlineUserCount
     user = User(uid: "FakeId", email: "hungry@person.food")
-    let completed = ref.observe(.value) { snapshot in
-      // 2
-      var newItems: [Recording] = []
-      // 3
-      for child in snapshot.children {
-        // 4
-        if
-          let snapshot = child as? DataSnapshot,
-          let groceryItem = Recording(snapshot: snapshot) {
-          newItems.append(groceryItem)
+    
+    let name = book.title
+    let query = ref.queryOrdered(byChild: "name").queryEqual(toValue: name)
+    print(query)
+    let completed = query.observe(.value, with: { (snapshot) in
+        var newItems: [Recording] = []
+        for child in snapshot.children {
+          // 4
+          if
+            let snapshot = child as? DataSnapshot,
+            let item = Recording(snapshot: snapshot) {
+            newItems.append(item)
+          }
         }
-      }
-      self.items = newItems
-      self.tableView.reloadData()
-      
-    }
+        self.items = newItems
+        self.tableView.reloadData()
+    })
+//
+//    let completed = ref.observe(.value) { snapshot in
+//      // 2
+//      var newItems: [Recording] = []
+//      // 3
+//      for child in snapshot.children {
+//        // 4
+//        if
+//          let snapshot = child as? DataSnapshot,
+//          let groceryItem = Recording(snapshot: snapshot) {
+//          newItems.append(groceryItem)
+//        }
+//      }
+//      self.items = newItems
+//      self.tableView.reloadData()
+//
+//    }
     refObservers.append(completed)
   }
 
@@ -119,30 +138,55 @@ class RecordingsViewController: UITableViewController {
 
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     guard let cell = tableView.cellForRow(at: indexPath) else { return }
-    var groceryItem = items[indexPath.row]
-//    let toggledCompletion = !groceryItem.completed
-//
-//    toggleCellCheckbox(cell, isCompleted: toggledCompletion)
-//    groceryItem.completed = toggledCompletion
+//    var groceryItem = items[indexPath.row]
+
     print("ROW IS SELECTED")
-    var url = Bundle.main.url(forResource: "sivastuthi", withExtension: "mp3")
-    if groceryItem.name == "Great Gatsby" {
-      url = Bundle.main.url(forResource: "greatgatsby_01_fitzgerald_64kb", withExtension: "mp3")
-    }
+    let url = items[indexPath.row].s3path
+    print(url)
+//    if groceryItem.name == "Great Gatsby" {
+//      url = Bundle.main.url(forResource: "greatgatsby_01_fitzgerald_64kb", withExtension: "mp3")
+//    }
         do {
-            audioplayer = try AVAudioPlayer(contentsOf: url!)
-            let player = audioplayer!
+//            audioplayer = try AVAudioPlayer(contentsOf: url!)
+//            let player = audioplayer!
+//            let playerItem:AVPlayerItem = AVPlayerItem(url: URL(string: url)!)
+          
+            avplayer = AVPlayer(url: URL(string: url)!)
+            
+            if !playing {
+              avplayer!.play()
+              playing = true
+            }
+            else {
+              avplayer!.pause()
+              playing = false
+            }
+//            player.prepareToPlay()
+//            player.play()
 
-            player.prepareToPlay()
-            player.play()
-
-        } catch let error as NSError {
-            print(error.description)
         }
+//        catch let error as NSError {
+//            print(error.description)
+//        }
     
     tableView.reloadData()
   }
-
+  
+//  @objc func playButtonTapped(_ sender:UIButton)
+//  {
+//      if player?.rate == 0
+//      {
+//          player!.play()
+//          //playButton!.setImage(UIImage(named: "player_control_pause_50px.png"), forState: UIControlState.Normal)
+//          playButton!.setTitle("Pause", for: UIControl.State.normal)
+//      } else {
+//          player!.pause()
+//          //playButton!.setImage(UIImage(named: "player_control_play_50px.png"), forState: UIControlState.Normal)
+//          playButton!.setTitle("Play", for: UIControl.State.normal)
+//      }
+//  }
+//
+//}
   func toggleCellCheckbox(_ cell: UITableViewCell, isCompleted: Bool) {
     if !isCompleted {
       cell.accessoryType = .none
